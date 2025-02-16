939a3d7e8f60dad8bcd2a437feaee8814c7626a7
Yasen El Makioui Benyagoub <workspaceyasen@gmail.com>
2025-02-16 22:54:05 +0100

    Initializing the server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "gosplash!!")
+	})
+
+	http.ListenAndServe(":8080", mux)
 }
